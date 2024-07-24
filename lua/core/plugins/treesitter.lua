a518b2a7093954eca426877b6bcb9826b82c1e59
gustavclausen <gustav@gustavclausen.com>
2024-07-24 20:09:42 +0200

    fix: treesitter installation

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -3,23 +3,29 @@ local settings = require("core.settings")
 local treesitter_languages = {}
 
 for _, lan in pairs(settings.languages) do
-  if not lan.treesitter == nil then
+  if not (lan.treesitter == nil) then
     table.insert(treesitter_languages, lan.treesitter)
   end
 end
+
+table.insert(treesitter_languages, "vim")
+table.insert(treesitter_languages, "vimdoc")
 
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   event = "BufReadPost",
+  lazy = false,
   dependencies = {
     "windwp/nvim-ts-autotag",
   },
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = treesitter_languages,
+      sync_install = false,
       highlight = {
         enable = true,
+        additional_vim_regex_highlighting = false,
       },
       indent = { enable = true },
       autotag = { enable = true },
